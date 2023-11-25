1c9511d167a9dcac47698b86c197a36250df3afa
Kushagra Saxena <devkushagra2002@gmail.com>
2023-11-25 16:58:56 +0530

    user visit notification changes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors'); // Import cors
 const userAgent = require('express-useragent'); // Import express-useragent
 const env = require('dotenv').config();
+const path = require('path'); // Add this line to import the 'path' module
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +15,45 @@ app.use(userAgent.express()); // Use express-useragent middleware
 const userName = env.parsed.username
 const passWord = env.parsed.password
 const receiverEmail = env.parsed.receiver_email
+
+app.post('/userDetails', async (req, res) => {
+    const { name, organization } = req.body;
+
+    const userInfo = {
+        platform: req.useragent.platform,
+        type: req.useragent.isDesktop ? 'Desktop' : req.useragent.isTablet ? 'Tablet' : 'Mobile',
+        browser: req.useragent.browser,
+        version: req.useragent.version,
+        osVersion: req.useragent.os,
+        architecture: req.useragent.is64bit ? 'x64' : 'x86'
+    };
+
+    // Set up Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: userName,
+            pass: passWord
+        }
+    });
+
+    // Email content
+    const mailOptions = {
+        from: userName,
+        to: receiverEmail,
+        subject: 'User Visit Notification',
+        text: `A user named ${name} from ${organization} organization visited your website.\nUser Info:\nIP Address: ${req.ip}\nTimestamp: ${new Date()}\n${JSON.stringify(userInfo, null, 2)}`
+    };
+
+    try {
+        // Send email
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'User details and notification email sent successfully.' });
+    } catch (error) {
+        console.error('Error sending user details and notification email:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.post('/contact', async (req, res) => {
     const { username, email, subject, message } = req.body;
@@ -70,7 +110,7 @@ app.post('/sendNoti', async (req, res) => {
         from: userName, // Replace with your email address
         to: receiverEmail, // Replace with your email address
         subject: 'User Visit Notification',
-        text: `Hello,\n\nA user visited your website.\n\nUser Agent: ${JSON.stringify(req.useragent, null, 2)}\nIP Address: ${req.ip}\nTimestamp: ${new Date()}\nUser Info: ${JSON.stringify(userInfo, null, 2)}`
+        text: `Hello,\n\nA user visited your website.\nUser Info:\nIP Address: ${req.ip}\nTimestamp: ${new Date()}\n${JSON.stringify(userInfo, null, 2)}`
     };
 
     try {
@@ -81,6 +121,12 @@ app.post('/sendNoti', async (req, res) => {
         console.error('Error sending notification email:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+});
+
+const indexPath = path.join(__dirname, '../index.html');
+
+app.get('/', (req, res) => {
+    res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
